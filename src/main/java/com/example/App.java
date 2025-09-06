aa8cfd1f381b226b82aae36fa18fcaaf2992841d
Premnath Narayanan <premnathknarayanan@mac.lan>
2025-09-06 08:50:04 +0100

    encapulation example

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,11 +1,17 @@
 package com.example;
 
+import com.example.components.Car;
+
 /**
  * Main application class for our Java OOAD project.
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) {        
         System.out.println("Welcome to Java Object Oriented Analysis and Design (OOAD) Project!");
         System.out.println("Project is up and running!");
+        Car myCar = new Car("Toyota");
+        System.out.println("My car name is " + myCar.getName());
+        myCar.setName("Toyota Corolla");
+        System.out.println("My car name is " + myCar.getName());        
     }
 }
